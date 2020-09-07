93a70b251aa9b54f838852d53af4833d80185126
Buck Perley <bucko.perley@gmail.com>
2020-09-07 15:18:44 -0500

    adds filters and fixes some type errors

--- src/components/charts/HistoricComparisonLineChart.tsx
+++ src/components/charts/HistoricComparisonLineChart.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
+import moment from 'moment';
 import {
   XAxis,
   YAxis,
@@ -12,6 +12,14 @@ import {
   Brush,
 } from "recharts"
 import randomColor from 'randomcolor';
+import {
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  Grid
+} from "@material-ui/core";
+
 import { getPerMPop } from "../../utils/utils";
 import { LineChartComparisonData, LineChartDataNode } from "../../types/charts";
 
@@ -19,10 +27,30 @@ interface ComparisonLineChartProps {
   comparisonData: LineChartComparisonData[],
   comparitor: string;
   perM?: boolean;
+  filter?: boolean
 }
 
-const HistoricComparisonLineChart = ({ comparisonData, comparitor, perM }: ComparisonLineChartProps) => {
+const HistoricComparisonLineChart = ({
+  comparisonData,
+  comparitor,
+  perM,
+  filter = true
+}: ComparisonLineChartProps) => {
   const locations = comparisonData.map(({ location }) => location)
+  
+  let stateObject: { [key: string]: boolean } = {}
+  const initialState = locations.reduce(
+    (state, location) => ({ ...state, [location]: true }),
+    stateObject
+  )
+  const [state, setState] = useState(initialState)
+  
+  const handleChange = (event: React.ChangeEvent) => {
+    const target = event.target as HTMLInputElement
+    setState({ ...state, [target.name]: target.checked })
+  }
+
+
   comparisonData = [...comparisonData]
   const firstLocation = comparisonData.shift();
   if (!firstLocation) return null;
@@ -32,7 +60,7 @@ const HistoricComparisonLineChart = ({ comparisonData, comparitor, perM }: Compa
   const data = firstLocation.data.map((node): LineChartDataNode => {
     const date = node.date;
     const data = {
-      date: node.date,
+      date: moment(node.date.toString()).format('MMM D'),
       [firstLocation.location]: perM ?
         getPerMPop(firstLocation.population, +node[comparitor]) :
         node[comparitor]
@@ -54,28 +82,52 @@ const HistoricComparisonLineChart = ({ comparisonData, comparitor, perM }: Compa
   })
 
   return (
-    <ResponsiveContainer width="100%" aspect={2}>
-      <LineChart data={data}>
-        {
-          locations.map((location: string) => (
-            <Line 
-              type='basisOpen'
+    <div>
+      {filter && locations.length > 2 ? (
+        <FormControl component="fieldset">
+            <FormGroup>
+              <Grid container>
+                {locations.map(location => (
+                  <Grid item key={location}>
+                    <FormControlLabel
+                    control={
+                      <Checkbox
+                      checked={state[location]}
+                      onChange={handleChange}
+                      name={location}
+                      />
+                    }
+                    label={location.toUpperCase()}
+                    />
+                </Grid>
+                ))}
+              </Grid>
+            </FormGroup>
+          </FormControl>
+      ) : null}
+      <ResponsiveContainer width="100%" aspect={2}>
+        <LineChart data={data}>
+          {locations
+            .filter(location => state[location])
+            .map((location: string) => (
+            <Line
+              type="basisOpen"
               key={location}
               dataKey={location.toLowerCase()}
-              stroke={randomColor({seed: location})}
+              stroke={randomColor({ seed: location })}
               dot={false}
               name={location.toUpperCase()}
             />
-          ))
-        }
-        <XAxis dataKey="date" />
-        <YAxis />
-        <CartesianGrid />
-        <Legend />
-        <Tooltip />
-        <Brush />
-      </LineChart>
-    </ResponsiveContainer>
+          ))}
+          <XAxis dataKey="date" />
+          <YAxis />
+          <CartesianGrid />
+          <Legend />
+          <Tooltip />
+          <Brush />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
